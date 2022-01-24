66a079db40b8b64e51696626a86300cca6762d2a
Evgenii.Korchagov <joedraiser@gmail.com>
2022-01-24 10:49:02 +0300

    21.01 amendments done

--- main.cpp
+++ main.cpp
@@ -3,16 +3,40 @@
 #include <mutex>
 
 std::mutex green_light;
+std::mutex coutMutex;
+
 int trains_arrived=0;
 
 void parovoz(int duration, char name)
 {
+    coutMutex.lock();
     std::cout << "Train " << name << " started moving to railway station\n";
+    coutMutex.unlock();
     std::this_thread::sleep_for(std::chrono::seconds(duration));
+    coutMutex.lock();
     std::cout << "Train " << name << " is waiting for free way to arrive\n";
+    coutMutex.unlock();
     green_light.lock();
+    coutMutex.lock();
     std::cout << "Train " << name << " arrived at the station\n";
+    coutMutex.unlock();
     trains_arrived++;
+}
+
+void trafficLight()
+{
+    std::string input;
+    while(true)
+    {
+        coutMutex.lock();
+        std::cout << "Type 'depart' to free the way\n";
+        coutMutex.unlock();
+        std::cin >> input;
+        if(input=="depart")
+        {
+            green_light.unlock();
+        }
+    }
 }
 
 int main()
@@ -26,23 +50,12 @@ int main()
         std::cin >> duration[i];
     }
 
-    std::cout << "Type 'depart' when to free the way\n";
-
     std::thread train1(parovoz, duration[0], name[0]);
     std::thread train2(parovoz, duration[1], name[1]);
     std::thread train3(parovoz, duration[2], name[2]);
+    std::thread svetofor(trafficLight);
 
-    std::string input;
-
-    while(trains_arrived!=2)
-    {
-        std::cin >> input;
-        if(input=="depart")
-        {
-            green_light.unlock();
-        }
-    }
-
+    svetofor.detach();
     train1.join();
     train2.join();
     train3.join();
